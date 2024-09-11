85a40ab8b48248baa398bfc5ece4d6c54742f110
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2024-09-12 01:10:50 +0700

    FirstCommit

---
+++ Semestr_3_Lab_0_WPF/Semestr_3_Lab_0_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Semestr_3_Lab_0_WPF.Views;
+using System.Windows.Input;
+
+namespace Semestr_3_Lab_0_WPF.ViewModels
+{
+    class MainWindowViewModel : ObservableObject
+    {
+        public ICommand OpenSecondControlCommand { get; set; }
+        public ICommand OpenFirstControlCommand { get; set; }
+
+        private object? _currentView;
+        private SecondControl _secondControl;
+        private FirstControl _firstControl;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                SetProperty(ref _currentView, value);
+                (OpenFirstControlCommand as MyDelegateCommand)?.InvokeCanExecuteChanged();
+                (OpenSecondControlCommand as MyDelegateCommand)?.InvokeCanExecuteChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            _firstControl = new FirstControl();
+            _secondControl = new SecondControl();
+
+            CurrentView = _firstControl;
+
+            OpenSecondControlCommand = new MyDelegateCommand(OpenSecondControl, CanOpenSecondControl);
+            OpenFirstControlCommand = new MyDelegateCommand(OpenFirstControl, CanOpenFirstControl);
+
+        }
+
+        private void OpenSecondControl(object parameter)
+        {
+            CurrentView = _secondControl;
+        }
+
+        private bool CanOpenSecondControl(object parameter)
+        {
+            return CurrentView != _secondControl;
+
+        }
+        private void OpenFirstControl(object parameter)
+        {
+            CurrentView = _firstControl;
+        }
+
+        private bool CanOpenFirstControl(object parameter)
+        {
+            return CurrentView != _firstControl;
+        }
+    }
+}
---
+++ Semestr_3_Lab_0_WPF/Semestr_3_Lab_0_WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Semestr_3_Lab_0_WPF.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semestr_3_Lab_0_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
