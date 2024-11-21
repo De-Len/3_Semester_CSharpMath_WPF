919b9bd0b2bfb0e3e56d09bb59f632c7549a7f3f
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2024-11-21 13:04:13 +0700

    Init

---
+++ 3_Semester_CSharpMath_WPF/3_Semester_CSharpMath_WPF/ViewModels/Pages/CoordinateDescentMethodPage/UserControls/Chart3DViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Semester_CSharpMath_WPF.ViewModels.Pages.CoordinateDescentMethodPage.UserControls
+{
+    internal class Chart3DViewModel
+    {
+
+    }
+}
---
+++ 3_Semester_CSharpMath_WPF/3_Semester_CSharpMath_WPF/ViewModels/Pages/SortingMethodsPage/SortingMethodsPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Semester_CSharpMath_WPF.ViewModels.Pages.SortingMethodsPage
+{
+    public class SortingMethodsPageViewModel : ObservableObject
+    {
+        public ObservableCollection<Person> DataGrid { get; set; }
+        public SortingMethodsPageViewModel()
+        {
+            DataGrid = new ObservableCollection<Person>
+            {
+                new Person { IsSelected = true, SortMethodName = "Пузырьковая", Timing = 0},
+                new Person { IsSelected = true, SortMethodName = "Вставками", Timing = 0 },
+                new Person { IsSelected = true, SortMethodName = "Быстрая", Timing = 0 },
+                new Person { IsSelected = true, SortMethodName = "Шейкерная", Timing = 0 },
+                new Person { IsSelected = true, SortMethodName = "Быстрая", Timing = 0 }
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string SortMethodName { get; set; }
+        public int Timing { get; set; }
+        public bool IsSelected {  get; set; }
+    }
+}
