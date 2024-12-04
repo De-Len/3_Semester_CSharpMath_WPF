d63603347822ffcebb0685e6473ee105b7212a5a
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2024-12-04 12:22:58 +0700

    Done 1.1

---
+++ 3_Semester_CSharpMath_WPF/3_Semester_CSharpMath_WPF/ViewModels/Pages/SortingMethodsPage/SortingMethodsPageViewModel.cs
@@ -0,0 +1,99 @@
+﻿using _3_Semester_CSharpMath_WPF.ViewModels.Pages.SortingMethodsPage.Windows.UserControls;
+using _3_Semester_CSharpMath_WPF.Views.Pages.SortingMethodsPage.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Ookii.Dialogs.Wpf;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace _3_Semester_CSharpMath_WPF.ViewModels.Pages.SortingMethodsPage
+{
+    partial class SortingMethodsPageViewModel : ObservableObject
+    {
+        public ICommand OpenSortingWindowCommand { get; }
+        public ICommand OpenFolderSelectionCommand { get; }
+        public ICommand OpenDataGeneratorWindowCommand { get; }
+
+        public static ObservableCollection<DataGridSortColumn> DataGrid { get; set; }
+
+        private SortingMethodsDataGeneratorWindowView _sortingMethodsDataGeneratorWindowView;
+
+        [ObservableProperty]
+        private string _bubbleSortOutput;
+
+        [ObservableProperty]
+        private string _insertionSortOutput;
+
+        [ObservableProperty]
+        private string _cocktailShakerSortOutput;
+
+        [ObservableProperty]
+        private string _quickSortOutput;
+
+        [ObservableProperty]
+        private string _bogoSortOutput;
+
+
+        public SortingMethodsPageViewModel()
+        {
+            DataGrid = new ObservableCollection<DataGridSortColumn>
+            {
+                new DataGridSortColumn { IsSelected = true, SortMethodName = "Пузырьковая", Timing = 0},
+                new DataGridSortColumn { IsSelected = true, SortMethodName = "Вставками", Timing = 0 },
+                new DataGridSortColumn { IsSelected = true, SortMethodName = "Шейкерная", Timing = 0 },
+                new DataGridSortColumn { IsSelected = true, SortMethodName = "Быстрая", Timing = 0 },
+                new DataGridSortColumn { IsSelected = true, SortMethodName = "BOGO", Timing = 0 }
+            };
+
+            OpenSortingWindowCommand = new RelayCommand(OpenSortingWindow);
+            OpenFolderSelectionCommand = new RelayCommand(OpenFolderSelection);
+            OpenDataGeneratorWindowCommand = new RelayCommand(OpenDataGeneratorWindow);
+        }
+
+        public void OpenSortingWindow()
+        {
+            var asd = BubbleSortOutput;
+            SortingMethodChooseSortingWindowsView sortingMethodChooseSortingWindowsView = new SortingMethodChooseSortingWindowsView(this);
+            sortingMethodChooseSortingWindowsView.Show();
+        }
+        public void OpenFolderSelection()
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                string folderPath = dialog.SelectedPath;
+                DataGeneratorAutomaticallyViewModel.FilePath = folderPath;
+            }
+        }
+        public void OpenDataGeneratorWindow()
+        {
+            _sortingMethodsDataGeneratorWindowView = new SortingMethodsDataGeneratorWindowView();
+            _sortingMethodsDataGeneratorWindowView.Show();
+        }
+    }
+
+    public class DataGridSortColumn : ObservableObject
+    {
+        private string _sortMethodName;
+        private long _timing;
+        private bool _isSelected;
+
+        public string SortMethodName
+        {
+            get => _sortMethodName;
+            set => SetProperty(ref _sortMethodName, value);
+        }
+
+        public long Timing
+        {
+            get => _timing;
+            set => SetProperty(ref _timing, value);
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetProperty(ref _isSelected, value);
+        }
+    }
+}
---
+++ 3_Semester_CSharpMath_WPF/3_Semester_CSharpMath_WPF/Views/Pages/CoordinateDescentMethodPage/UserConrtol/Chart3DView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using _3_Semester_CSharpMath_WPF.ViewModels.Pages.CoordinateDescentMethodPage.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_Semester_CSharpMath_WPF.Views.Pages.CoordinateDescentMethodPage.UserConrtol
+{
+    /// <summary>
+    /// Interaction logic for Chart3DView.xaml
+    /// </summary>
+    public partial class Chart3DView : UserControl
+    {
+        internal Chart3DViewModel Chart3DViewModel;
+        public Chart3DView()
+        {
+            Chart3DViewModel = new Chart3DViewModel();
+            InitializeComponent();
+        }
+    }
+}
